e372b1c61a4396ca7a974ddbc7ada12af416fccd
Thomas Beranek <52349197+beranek1@users.noreply.github.com>
2019-07-28 13:31:58 +0200

    Update webstatistics.php

--- webstatistics.php
+++ webstatistics.php
@@ -232,9 +232,11 @@ foreach($web_analytics_db->query("SELECT `uri`, COUNT(*) FROM wa_requests GROUP 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
-    <footer>
-        <a href="https://webanalytics.one">Powered by WebAnalytics</a>
-    </footer>
+    <nav class="navbar navbar-light bg-light">
+        <span class="navbar-text">
+                <a href="https://webanalytics.one">Powered by WebAnalytics</a>
+        </span>
+    </nav>
 </html>
 <?php
 /* Classes */
