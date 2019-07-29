936ee8f74ea59cf5c3899a4e50c4d99e723f39c1
Thomas Beranek <52349197+beranek1@users.noreply.github.com>
2019-07-29 20:39:27 +0200

    Update webstatistics.php

--- webstatistics.php
+++ webstatistics.php
@@ -32,7 +32,6 @@ if($total_requests == 0) {
 }
 $total_visitors = $web_analytics_db->count("wa_browsers");
 $total_networks = $web_analytics_db->count("wa_ips");
-$total_isps = $web_analytics_db->count("wa_isps");
 $top_countries = array();
 $top_continents = array();
 $total_continents = 0;
@@ -63,7 +62,7 @@ foreach($web_analytics_db->query("SELECT `country`, COUNT(*) FROM wa_browsers GR
 $top_languages = array();
 $total_languages = 0;
 foreach($tplngsr = $web_analytics_db->query("SELECT `language`, COUNT(*) FROM wa_browsers GROUP BY `language` ORDER BY COUNT(*) DESC;") as $language) {
-    if($language != "" && $language != null) {
+    if($language[0] != "" && $language[0] != null) {
         $top_languages[$language[0]] = $language[1];
         $total_languages = $total_languages + 1;
     } else {
@@ -74,9 +73,15 @@ $top_useragents = array();
 foreach($web_analytics_db->query("SELECT `user_agent`, COUNT(*) FROM wa_browsers GROUP BY `user_agent` ORDER BY COUNT(*) DESC;") as $useragent) {
     $top_useragents[$useragent[0]] = $useragent[1];
 }
+$total_isps = 0;
 $top_isps = array();
-foreach($web_analytics_db->query("SELECT `isp_id`, COUNT(*) FROM wa_ips GROUP BY `isp_id` ORDER BY COUNT(*) DESC;") as $isp) {
-    $top_isps[$isp[0]] = $isp[1];
+foreach($web_analytics_db->query("SELECT `isp`, COUNT(*) FROM wa_ips GROUP BY `isp` ORDER BY COUNT(*) DESC;") as $isp) {
+    if($isp[0] != "" && $isp[0] != null) {
+        $top_isps[$isp[0]] = $isp[1];
+        $total_isps++;
+    } else {
+        $top_isps["?"] = $isp[1];
+    }
 }
 $top_uris = array();
 foreach($web_analytics_db->query("SELECT `uri`, COUNT(*) FROM wa_requests GROUP BY `uri` ORDER BY COUNT(*) DESC;") as $uri) {
